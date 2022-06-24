4cdbbf48511ea8653259853bcca01fa49266053e
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-06-24 00:08:47 -0400

    Close any running instances of LiveSplit before upgrade/uninstall

---
+++ liveSplit/tools/chocolateyBeforeModify.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$processName = 'LiveSplit'
+$processes = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ($null -ne $processes)
+{
+  Write-Warning "$processName is currently running, stopping it to prevent upgrade/uninstall from failing..."
+
+  foreach ($process in $processes)
+  {
+    Stop-Process -InputObject $process -ErrorAction SilentlyContinue
+
+    Start-Sleep -Seconds 3
+
+    if (!$process.HasExited)
+    {
+        Write-Warning "$processName (PID: $($process.Id)) is still running despite stop request, force stopping it..."
+        Stop-Process -InputObject $process -Force -ErrorAction SilentlyContinue
+    }
+  }
+
+  Write-Warning "If upgrading, $processName may need to be manually restarted upon completion"
+}
+else
+{
+  Write-Debug "No running $processName process instances were found"
+}
